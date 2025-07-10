7782277568276ef4da8b086016b557353899f556
gitabdul0001 <tonmoy45557@gmail.com>
2025-07-11 01:40:50 +0600

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    host: true,
+    port: 5173,
+    hmr: {
+      overlay: false
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ethers: ['ethers']
+        },
+      },
+    },
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
+});
